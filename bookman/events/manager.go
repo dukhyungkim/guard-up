c677a17650a19b31e7fe11dad4e5fee469d78d90
DukHyung Kim <idealismboy@gmail.com>
2022-07-04 16:59:13 +0900

    done v2 event

--- bookman/events/manager.go
+++ bookman/events/manager.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 )
 
 var eventManagerInstance *EventManager
@@ -13,6 +14,17 @@ var eventManagerInstance *EventManager
 type Event struct {
 	Type EventType
 	Data string
+}
+
+func (e *Event) ToWebsocketEvent() *WebsocketEvent {
+	wsEvent := WebsocketEvent{Type: e.Type}
+	_ = json.Unmarshal([]byte(e.Data), &wsEvent.Data)
+	return &wsEvent
+}
+
+type WebsocketEvent struct {
+	Type EventType      `json:"type"`
+	Data map[string]any `json:"data"`
 }
 
 type EventManager struct {
@@ -55,6 +67,14 @@ func (m *EventManager) HandleEvent() {
 	}
 }
 
+func (m *EventManager) SendEvent(eventType EventType, eventData any) {
+	data, _ := json.Marshal(eventData)
+	m.Message <- Event{
+		Type: eventType,
+		Data: string(data),
+	}
+}
+
 func (m *EventManager) HandleSSE(c *gin.Context) {
 	c.Writer.Header().Set("Content-Type", "text/event-stream")
 	c.Writer.Header().Set("Cache-Control", "no-cache")
@@ -75,10 +95,47 @@ func (m *EventManager) HandleSSE(c *gin.Context) {
 	})
 }
 
-func (m *EventManager) SendEvent(eventType EventType, eventData any) {
-	data, _ := json.Marshal(eventData)
-	m.Message <- Event{
-		Type: eventType,
-		Data: string(data),
+func (m *EventManager) HandleWebsocketNotify(upgrader websocket.Upgrader) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		stream := make(chan Event)
+		m.NewClient <- stream
+
+		defer func() {
+			if err = ws.Close(); err != nil {
+				log.Println(err)
+			}
+
+			m.CloseClient <- stream
+			close(stream)
+		}()
+
+		closeChan := make(chan bool)
+		go func() {
+			_, _, err := ws.ReadMessage()
+			if err != nil {
+				log.Println(err)
+				closeChan <- true
+			}
+		}()
+
+	LOOP:
+		for {
+			select {
+			case <-closeChan:
+				log.Println("got closed")
+				break LOOP
+			case msg := <-stream:
+				err = ws.WriteJSON(msg.ToWebsocketEvent())
+				if err != nil {
+					log.Println(err)
+					break LOOP
+				}
+			}
+		}
 	}
 }
--- bookman/router/v2/v2.go
+++ bookman/router/v2/v2.go
@@ -8,7 +8,6 @@ import (
 	"bookman/service"
 	"encoding/json"
 	"log"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
@@ -41,7 +40,7 @@ func SetupRouter(cfg *config.Config, r *gin.Engine, eventManager *events.EventMa
 	upgrader := websocket.Upgrader{}
 
 	v2GroupRouter.GET("action", newActionHandler(upgrader, bookHandler, userHandler))
-	v2GroupRouter.GET("event", newEventHandler(upgrader))
+	v2GroupRouter.GET("event", eventManager.HandleWebsocketNotify(upgrader))
 }
 
 func newActionHandler(upgrader websocket.Upgrader, bookHandler *BookHandler, userHandler *UserHandler) func(c *gin.Context) {
@@ -122,54 +121,6 @@ func newActionHandler(upgrader websocket.Upgrader, bookHandler *BookHandler, use
 			if err != nil {
 				log.Println(err)
 				break
-			}
-		}
-	}
-}
-
-func newEventHandler(upgrader websocket.Upgrader) func(c *gin.Context) {
-	return func(c *gin.Context) {
-		ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		defer func() {
-			if err = ws.Close(); err != nil {
-				log.Println(err)
-				return
-			}
-		}()
-
-		closeChan := make(chan bool)
-		go func() {
-			_, _, err := ws.ReadMessage()
-			if err != nil {
-				log.Println(err)
-				closeChan <- true
-			}
-		}()
-
-	LOOP:
-		for {
-			select {
-			case <-closeChan:
-				log.Println("got closed")
-				break LOOP
-			default:
-				pong := struct {
-					Message string `json:"message"`
-				}{
-					Message: "pong",
-				}
-				b, _ := json.Marshal(pong)
-
-				err = ws.WriteMessage(websocket.TextMessage, b)
-				if err != nil {
-					log.Println(err)
-					break LOOP
-				}
-				time.Sleep(time.Second)
 			}
 		}
 	}
